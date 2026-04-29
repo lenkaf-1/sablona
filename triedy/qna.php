6112dc3631473ca71b0005ac822602a83fbb2a7e
roseuniverse7411 <lenkafilipova741@gmail.com>
2026-04-29 21:38:47 +0200

    Update qna.php

--- triedy/qna.php
+++ triedy/qna.php
@@ -2,10 +2,16 @@
 
 namespace App;
 
+require __DIR__ . '/database.php';
+
 class QnA extends Database
 {
     public function getAll(): array
     {
-        return $this->loadData();
+        $data = $this->loadData();
+        if (!is_array($data)) {
+            throw new \Exception('Chyba: Neplatné dáta načítané z databázy.');
+        }
+        return $data;
     }
 }
